c8987b5ae9e6abcf42a030e9a06cc3be4934d262
Jean de Klerk <jadekler@gmail.com>
2015-03-08 18:53:52 -0600

    Working json parsing

--- rest_test.cc
+++ rest_test.cc
@@ -1,40 +1,49 @@
 #include <iostream>
 #include <string>
-#include <curl/curl.h> //your directory may be different
+#include <curl/curl.h>
+#include "lib/json-parser/json.h"
+
+
 using namespace std;
 
 string data; //will hold the url's contents
 
-size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up)
-{ //callback must have this declaration
+size_t writeCallback(char *buf, size_t size, size_t nmemb, void *up) { //callback must have this declaration
     //buf is a pointer to the data that curl has for us
     //size*nmemb is the size of the buffer
 
-    for (int c = 0; c<size*nmemb; c++)
-    {
+    for (int c = 0; c < size * nmemb; c++) {
         data.push_back(buf[c]);
     }
-    return size*nmemb; //tell curl how many bytes we handled
+    return size * nmemb; //tell curl how many bytes we handled
 }
 
-int main()
-{
-    CURL* curl; //our curl object
+int main() {
+//    CURL *curl; //our curl object
+//
+//    curl_global_init(CURL_GLOBAL_ALL); //pretty obvious
+//    curl = curl_easy_init();
+//
+//    curl_easy_setopt(curl, CURLOPT_URL, "http://pingpongping.cfapps.io/activity");
+//    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
+//    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
+//
+//    curl_easy_perform(curl);
+//
+//    cout << endl << data << endl;
+////    cin.get();
+//
+//    curl_easy_cleanup(curl);
+//    curl_global_cleanup();
 
-    curl_global_init(CURL_GLOBAL_ALL); //pretty obvious
-    curl = curl_easy_init();
+    json_char *input = "{\"boom\":7, \"sdk\":\"jdk\"}";
 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://pingpongping.cfapps.io/activity");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
+    json_value *res = json_parse(input, strlen(input));
+    printf("\n%s\n", res->u.object.values[0].name);
+    printf("\n%d\n", res->u.object.values[0].value->u.integer);
 
-    curl_easy_perform(curl);
-
-    cout << endl << data << endl;
-    cin.get();
-
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
+    printf("\n%s\n", res->u.object.values[1].name);
+    printf("\n%s\n", res->u.object.values[1].value->u.string.ptr);
 
     return 0;
 }
