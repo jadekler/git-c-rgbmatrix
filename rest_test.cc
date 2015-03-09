508c79c2ca5d33017db4a806e5c380122e621736
Jean de Klerk <jadekler@gmail.com>
2015-03-08 20:07:46 -0600

    Working time parsing to seconds

--- rest_test.cc
+++ rest_test.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <curl/curl.h>
+#include <sys/time.h>
 #include "lib/json-parser/json.h"
 
 using namespace std;
@@ -12,21 +12,34 @@ void performGet();
 int main() {
     performGet();
 
+    bool activity[32];
+    for (int i = 0; i < 32; i++) {
+        activity[i] = false;
+    }
+
     char const *input = data.c_str();
+    json_value jsonParsedInput = * json_parse(input, strlen(input));
 
-    json_value res = * json_parse(input, strlen(input));
+    for (int i = 0; i < jsonParsedInput.u.array.length; i++) {
+        json_value *arrItem = jsonParsedInput.u.array.values[i];
 
-    json_value *arr = res.u.array.values[0];
+        bool isActive = arrItem->u.object.values[0].value->u.boolean;
+        string activityDateString = arrItem->u.object.values[1].value->u.string.ptr;
+        activityDateString = activityDateString.substr(0, 19);                      // Remove microseconds
+        replace(activityDateString.begin(), activityDateString.end(), 'T', ' ');    // Remove T in middle
 
-    for (int i = 0; i < res.u.array.length; i++) {
-        json_value *arrItem = res.u.array.values[i];
-        cout << arrItem->u.object.values[1].value->u.string.ptr << " : " << arrItem->u.object.values[0].value->u.boolean << endl;
+        struct tm tm;
+        strptime(activityDateString.c_str(), "%Y-%m-%d %H:%M:%S", &tm);
+        time_t activityDateSeconds = mktime(&tm);
+
+        cout << activityDateString << " " << activityDateSeconds << endl;
     }
 
     return 0;
 }
 
-size_t writeCallback(char *buf, size_t size, size_t nmemb, void *up) { //callback must have this declaration
+size_t writeCallback(char *buf, size_t size, size_t nmemb, void *up) {
+    //callback must have this declaration
     //buf is a pointer to the data that curl has for us
     //size*nmemb is the size of the buffer
 
